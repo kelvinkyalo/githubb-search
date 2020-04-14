9ac64e82c65988a8e4cfc9aa760f5d518cb48103
kelvinkyalo <kelvinchax@gmail.com>
2020-04-14 13:57:59 +0300

    add http module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-// import { HttpModule } from '@angular/http';
+import { HttpModule } from '@angular/http';
 
-// import { BioService } from './services/bio.service'
+import { BioService } from './services/bio.service'
 // import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BioComponent } from './components/bio/bio.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
-    BioComponent
-    // HttpModule
+    BioComponent,
+    HttpModule
   ],
   imports: [
     BrowserModule,
     // AppRoutingModule
   ],
-  providers: [],
+  providers: [BioService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/services/bio.service.ts
+++ src/app/services/bio.service.ts
@@ -1,12 +1,24 @@
-// import { Injectable } from '@angular/core';
-// import { Http, Headers } from '@angular/http';
-// import 'rxjs/add/operator/map'; 
+import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import 'rxjs/add/operator/map';
 
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class BioService {
-//   getBioInfo: any;
+@Injectable({
+  providedIn: 'root'
+})
+export class BioService {
+  //   getBioInfo: any;
+  private username: string;
+  private clientid = 'eb2e72fd0b231f431b5b';
+  private clientsecret = '8cc2493fbf6129392085fefc07af8e11faa57fa2';
 
+  constructor(private http: Http) {
+    console.log('service ready');
+    this.username = 'kirandash';
 
-//   }
+  }
+
+  getProfileInfo(){
+    return this.http.get('https://api.github.com/users/' + this.username + "?client_id=" + this.clientid + "&client_secret" + this.clientsecret)
+    .map(res =>res.json()); 
+  }
+}
