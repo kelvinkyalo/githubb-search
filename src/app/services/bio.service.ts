34d45beead674b887715ad83953ea152b03c96e3
kelvinkyalo <kelvinchax@gmail.com>
2020-04-14 12:47:08 +0300

    remove injectible error

--- src/app/services/bio.service.ts
+++ src/app/services/bio.service.ts
@@ -7,16 +7,5 @@ import 'rxjs/add/operator/map';
 })
 export class BioService {
 
-   private username:string; 
-   private clientid: 'd07b7899161abd248a4f';
-   private clientsecret: '623d37f46325eef178d4e1f5913cf91cd861e28f';
-
-  constructor(private http:Http) {
-    console.log("service ready");
-    this.username = 'kelvinkyalo';
-   }
-   getBioInfo(){
-     return this.http.get("https://api.github.com/users/"+ this.username + "?client_id=" + this.clientid + "&client_secret=" + this.clientsecret)
-     .map(res => res.json);
-   }
-}
+  
+  }
