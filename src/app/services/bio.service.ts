09d513a439e6e372e6bbe511bf7cf3cae5cb1488
kelvinkyalo <kelvinchax@gmail.com>
2020-04-14 17:30:52 +0300

    fix  http error

--- src/app/services/bio.service.ts
+++ src/app/services/bio.service.ts
@@ -1,15 +1,15 @@
 import { Injectable } from '@angular/core';
-import { HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import 'rxjs/add/operator/map';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BioService {
+  //   getBioInfo: any;
   private username: string;
   private clientid = 'eb2e72fd0b231f431b5b';
   private clientsecret = '8cc2493fbf6129392085fefc07af8e11faa57fa2';
-  getBioInfo: any;
 
   constructor(private http: HttpClient) {
     console.log('service ready');
@@ -17,8 +17,9 @@ export class BioService {
 
   }
 
-  getProfileInfo(){
-    return this.http.get('https://api.github.com/users/' + this.username + "?client_id=" + this.clientid + "&client_secret" + this.clientsecret)
-    .map(res =>res.json()); 
+  getBioInfo(){
+    return this.http.get("https://api.github.com/users/" + this.username + "?client_id=" + this.clientid 
+    + "&client_secret=" + this.clientsecret)
+    .map(res =>res.json());
   }
 }
